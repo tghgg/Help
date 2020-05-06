c02c40519a44dc85ca01f8a87b0c2f50236d9402
Falling Snowdin <falling_snowdin@protonmail.com>
2020-05-06 13:51:37 +0700

    Finish solving second case

--- nibbawut.cpp
+++ nibbawut.cpp
@@ -11,15 +11,22 @@ double * giaiDaThuc(int bac, double heso[])
     switch (bac)
     {
         case 1:
-        {
+        {   
             nghiem[0] = (-heso[bac]) / heso[0];
             break;
         }
         case 2:
         {
-            if (heso[0] == 0)
-                cout << "He so a = 0\nPhuong trinh khong hop le.\n"; break;
-            double delta = pow(heso[1], 2.0) - 4 * heso[0] * heso[2];
+            const double a = heso[0];
+            if (a == 0) {
+                cout << "He so a = 0\nPhuong trinh khong hop le.\n";
+                break;
+            }
+            const double b = heso[1];
+            const double c = heso[2];
+    
+            double delta = pow(b, 2.0) - 4 * a * c;
+
             if (delta < 0.0)
             {
                 nghiem = nullptr;
@@ -27,11 +34,15 @@ double * giaiDaThuc(int bac, double heso[])
             }
             else if (delta == 0.0)
             {
-
+                nghiem[0] = (-b)/(2.0*a);
+                nghiem[1] = nghiem[0];
                 break;
             }
             else
             {
+                delta = sqrt(delta);
+                nghiem[0] = (-b + delta) / (2 * a);
+                nghiem[1] = (-b - delta) / (2 * a);
                 break;
             }
         }
@@ -46,7 +57,7 @@ int main()
 {
     int bac;
 
-    cout << "Phuong trinh bac may!?!??!\n";
+    cout << "Phuong trinh bac may?\n";
     cin >> bac;
 
     double *heso = new double[bac+1];
@@ -59,7 +70,7 @@ int main()
     }
 
     const double *nghiem = giaiDaThuc(bac, heso);
-
+    cout << *nghiem << endl;
     // Xuat nghiem
 
     if (nghiem == nullptr)
@@ -69,11 +80,12 @@ int main()
         for (unsigned int i = 0; i < bac; i++) {
             cout << "x" << i+1 << " = " << nghiem[i] << endl;
         }
+        delete nghiem;
+        nghiem = nullptr;
     }
 
     delete [] heso;
-    delete nghiem;
-    nghiem = nullptr;
+    
     heso = nullptr;
 
     return 0;
